ac12c84688e69bcf1d8365c9f6bdc296395fd963
Duje Nikolić <71044241+nduje@users.noreply.github.com>
2020-10-27 03:38:34 +0100

    Update and rename Zadatak 1.c to Zadatak 1/Zadatak 1.c

--- Zadatak 1/Zadatak 1.c
+++ Zadatak 1/Zadatak 1.c
@@ -2,9 +2,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<malloc.h>
-#define lName 64
+#define lName 64										//makronaredba
 
-typedef struct Student {
+typedef struct Student {								//struktura student
 	char ime[lName];
 	char prezime[lName];
 	int bodovi;
@@ -19,33 +19,33 @@ int main() {
 	int br = 0;											//br - brojac (koliko studenata ima u listi (koliko ima redaka))
 	Student *stud;
 	FILE *lista;
-	lista = fopen("lista.txt", "r");
+	lista = fopen("lista.txt", "r");					//otvaranje datoteke i stavljanje u mode za citanje iz nje
 
-	if (lista == NULL) {
+	if (lista == NULL) {								//provjera je li datoteka pravilno otvorena, ako nije ispisuje gresku i vraca -1
 		puts("Greska pri otvaranju datoteke!");
 		return -1;
 	}
 
-	rewind(lista);
+	rewind(lista);										//vraca cursor na pocetak datoteke
 
 	while (!feof(lista))
-		if (fgetc(lista) == '\n')
+		if (fgetc(lista) == '\n')						//na kraju datoteke mora biti prazan redak kako bi brojac dao tocnu vrijednost
 			br++;										//svaki put kada se ude u novi redak, vrsi se inkrementacija brojaca
 
 	printf("Lista se sastoji od %d studenata\n\n", br);
 
 	stud = (Student *)malloc(br * sizeof(Student));		//alociramo memoriju za niz struktura studenata
 
-	rewind(lista);
+	rewind(lista);										//vraca cursor na pocetak datoteke
 
-	for (i = 0; i < br; i++)
+	for (i = 0; i < br; i++)							//unos podataka (iz datoteke) u niz studenata
 		fscanf(lista, "%s %s %d", stud[i].ime, stud[i].prezime, &stud[i].bodovi);
 
-	relativni(br, stud);
+	relativni(br, stud);								//poziv funkcije koja racuna relativan broj bodova studenta
 
-	ispis(br, stud);
+	ispis(br, stud);									//poziv funkcije koja ispisuje listu studenata
 
-	fclose(lista);
+	fclose(lista);										//obavezno zatvaranje datoteke na kraju programa
 
 	return 0;
 }
@@ -55,13 +55,13 @@ void relativni(int br, Student *stud) {
 	int i;
 	float maxBodovi;									//maksimalan broj bodova
 
-	maxBodovi = (float) stud[0].bodovi;
+	maxBodovi = (float) stud[0].bodovi;					//cast operator (float) na broj bodova (koji je tipa int)
 
-	for (i = 0; i < br; i++)
+	for (i = 0; i < br; i++)							//trazi se maksimalan broj bodova, odnosno, koji je najveci broj postignutih bodova
 		if (stud[i].bodovi > maxBodovi)
 			maxBodovi = (float) stud[i].bodovi;
 
-	for (i = 0; i < br; i++)
+	for (i = 0; i < br; i++)							//racunanje relativnog broja bodova svakog studenta
 		stud[i].relBodovi = (float) stud[i].bodovi / maxBodovi * 100;
 
 }
@@ -71,7 +71,9 @@ void ispis(int br, Student *stud) {
 	int i;
 
 	printf("Ime		 Prezime		Apsolutni br. bodova		Relativni br. bodova\n");
-	for (i = 0; i < br; i++)
+	for (i = 0; i < br; i++)							//ispis liste studenata
 		printf("%-8s\t %-10s\t %18d\t %28.2f\n", stud[i].ime, stud[i].prezime, stud[i].bodovi, stud[i].relBodovi);
+
+	puts("");
 
 }
