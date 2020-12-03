06be45b997bee8245915aaa6f225b9ce23f39f7f
Duje Nikolić <71044241+nduje@users.noreply.github.com>
2020-12-03 16:33:50 +0100

    Update Zadatak 5.c
    Dodan dio koda u kojem se vrši unos prve i druge liste. Potrebno je još dodati funkcije koje računaju uniju i presjek.

--- Zadatak 5/Zadatak 5.c
+++ Zadatak 5/Zadatak 5.c
@@ -3,60 +3,83 @@
 #include<malloc.h>
 
 typedef struct Skupovi * Pozicija;
- 
-struct Skupovi{										//definiramo strukturu
+
+struct Skupovi {										//definiramo strukturu
 	int broj;
 	Pozicija next;
 };
 
+void Lista(Pozicija P);								
 void Unos(int broj, Pozicija P);					//vrsi se sortirani unos u datoteku
 void Ispis(Pozicija P);								//funkcija koja ispisuje listu
 int main() {
 	struct Skupovi Broj1, Broj2;
-	int temp = 0;									//privremena cjelobrojna varijabla koju saljemo u funkciju Unos()
+	int n = 0, i = 0;									//privremena cjelobrojna varijabla koju saljemo u funkciju Unos()
 	Broj1.next = NULL;
 	Broj2.next = NULL;
 
-	printf("Unesite clanove prve liste\n");
-	
-	while(getchar() != '\n') {						//petlja se vrti dok se umjesto unosa broja ne stisne enter
-		scanf("%d", &temp);
-		Unos(temp, &Broj1);
-	}
+	printf("Vrsi se unos prve liste.\n\n");
+
+	Lista(&Broj1);
 
 	printf("\nPrva lista glasi\n");
 	Ispis(Broj1.next);
 
-	printf("\nUnesite clanove druge liste\n");
-	
-	while(getchar() != 'n') {
-		scanf("%d", &temp);
-		Unos(temp, &Broj2);
-	}
+	printf("\n\nVrsi se unos druge liste.\n\n");
+
+	Lista(&Broj2);
 
 	printf("\nDruga lista glasi\n");
 	Ispis(Broj2.next);
+
+	puts("");
 
 	return 0;
 }
 
 
+void Lista(Pozicija P) {
+
+	int broj = 0, i = 1, n = 0;
+
+	printf("Koliko zelite da lista sadrzi clanova?\n");
+
+	scanf("%d", &n);
+
+	puts("");
+
+	while (n != 0) {
+
+		printf("Unesite %d. clan liste.\n", i);
+
+		scanf("%d", &broj);
+
+		Unos(broj, P);
+
+		i++;
+		n--;
+	}
+}
+
 void Unos(int broj, Pozicija P) {
+
 	Pozicija q;
 
 	q = (Pozicija)malloc(sizeof(struct Skupovi));
+
+	while (P->next != NULL && broj > P->next->broj)
+		P = P->next;
 
 	q->broj = broj;
 
 	q->next = P->next;
 	P->next = q;
-
 }
 
 
 void Ispis(Pozicija P) {
 
-	while(P != NULL) {
+	while (P != NULL) {
 		printf("%d ", P->broj);
 		P = P->next;
 	}
